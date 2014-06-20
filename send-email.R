6d0cd26c5685468c33690053052200b21875ab14
Brock Tibert <btibert3@gmail.com>
2014-06-20 07:15:04 -0400

    Update send-email.R

--- send-email.R
+++ send-email.R
@@ -4,8 +4,8 @@ knitr::knit2html('my-report.RMD', stylesheet = 'flat-table.css')
 ## send the HTML in an email using the gmail server
 ## assumes you are using GMAIL
 library(mailR)
-send.mail(from = "btibert3@gmail.com",
-          to = c("btibert@bentley.edu"),
+send.mail(from = "youremail@gmail.com",
+          to = c("another.email@gmail.com"),
           subject = "The Table report",
           body = "my-report.html",
           smtp = list(host.name = "smtp.gmail.com", 
